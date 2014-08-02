2f721fb528b5fdca1a5c6d9f2149b8f839449e42
Anthony Zacharakis <anthony@lumoslabs.com>
2014-08-02 16:02:21 -0700

    Add basic token authentication to api

--- Gemfile
+++ Gemfile
@@ -1,5 +1,6 @@
 gem 'rambling-trie', '0.6.1'
 gem 'sinatra',       '1.4.5'
+gem 'warden',        '1.2.3'
 
 group :development, :test do
   gem 'pry',   '~> 0.10'
--- app.rb
+++ app.rb
@@ -1,18 +1,56 @@
+require 'erb'
 require 'sinatra'
 require 'json'
+require 'warden'
+require 'yaml'
 require_relative 'phrases'
 
-PHRASES_FILE_PATH = '/tmp/test'
+class PhrasesApp < Sinatra::Base
+  CONF = YAML.load(File.read(File.join(File.expand_path(File.dirname(__FILE__)), 'conf.yml')))
 
-Phrases.initialize_phrases(PHRASES_FILE_PATH)
+  Phrases.initialize_phrases(CONF['phrases_file_path'])
 
-get '/api/phrase.json' do
-  content_type :json
-  {phrase: Phrases.get(params[:search])}.to_json
-end
+  get '/' do
+    'Welcome to Phrases!'
+  end
 
-post '/api/phrase.json' do
-  return 400 unless params[:phrase] && !params[:phrase].empty?
-  Phrases.add(params[:phrase])
-  {phrase: params[:phrase]}.to_json
+  post '/unauthenticated/?' do
+    status 401
+    request.env['warden'].custom_failure!
+  end
+
+  before '/api/*' do
+    request.env['warden'].authenticate!(:access_token)
+  end
+
+  get '/api/phrase.json' do
+    content_type :json
+    {phrase: Phrases.get(params[:search])}.to_json
+  end
+
+  post '/api/phrase.json' do
+    error 400 unless params[:phrase] && !params[:phrase].empty?
+    Phrases.add(params[:phrase])
+    {phrase: params[:phrase]}.to_json
+  end
+
+  Warden::Manager.before_failure do |env,opts|
+    env['REQUEST_METHOD'] = "POST"
+  end
+
+  use Warden::Manager do |config|
+    config.scope_defaults :default, strategies: [:access_token]
+    config.failure_app = self
+  end
+
+  Warden::Strategies.add(:access_token) do
+    def valid?
+      request.env['HTTP_ACCESS_TOKEN'].is_a?(String)
+    end
+
+    def authenticate!
+      access_granted = (request.env['HTTP_ACCESS_TOKEN'] == CONF['access_token'])
+      !access_granted ? fail! : success!(access_granted)
+    end
+  end
 end
